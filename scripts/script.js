2c24efd9bbb226b88b3105df62ecd2c39ab4bd5a
Anton Khlebnikov <toni.farsh@icloud.com>
2024-08-09 14:20:07 +0500

    fix script index

--- scripts/script.js
+++ scripts/script.js
@@ -5,14 +5,18 @@ function checkAgreem(item) {
     item.classList.remove('checked')
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        for (let i = 0; i < agrimentItems.length; i++) {
+            const element = agrimentItems[i];
+            setTimeout(() => {
+                checkAgreem(element);
+                if (i === agrimentItems.length - 1) {
+                    btnAction.classList.remove('disabled')
+                }
+            }, (i * 600) + 700);
 
-for (let i = 0; i < agrimentItems.length; i++) {
-    const element = agrimentItems[i];
-    setTimeout(()=> {
-        checkAgreem(element);
-        if( i === agrimentItems.length - 1) {
-            btnAction.classList.remove('disabled')
-        }
-    },(i * 600) + 700);
-    
-}
+        };
+    }, 1000);
+
+});
